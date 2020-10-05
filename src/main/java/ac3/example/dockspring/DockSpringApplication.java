1adb1198896cf79675d0b56b72c8ca560ccef78d
kevin ard <muhammad.ardela@dana.id>
2020-10-05 20:58:32 +0700

    Initial commit

---
+++ src/main/java/ac3/example/dockspring/DockSpringApplication.java
@@ -0,0 +1,13 @@
+package ac3.example.dockspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockSpringApplication.class, args);
+	}
+
+}
